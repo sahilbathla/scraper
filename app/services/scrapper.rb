673723b218fa669d5290d273ae4889867ad3cfe1
Udit Mittal <udit7590@users.noreply.github.com>
2019-06-10 19:32:21 +0530

    chrome issue

--- app/services/scrapper.rb
+++ app/services/scrapper.rb
@@ -33,7 +33,7 @@ class Scrapper
 
     Capybara.register_driver :headless_chrome do |app|
       capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
-          chromeOptions: { args: %w(headless disable-gpu) }
+          chromeOptions: { args: %w(headless no-sandbox disable-gpu) }
       )
 
       Capybara::Selenium::Driver.new app,
